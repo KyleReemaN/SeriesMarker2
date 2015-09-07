e649729e299f679e4bb23857b20ba75d8b35af55
KyleReemaN <tobias.watzka@student.uni-augsburg.de>
2015-09-07 14:59:48 +0200

    * Start adding series marker logic

---
+++ app/src/main/java/de/augsburg/seriesmarker2/Episode.java
@@ -0,0 +1,11 @@
+package de.augsburg.seriesmarker2;
+
+/**
+ * Created by Tobias on 07.09.2015.
+ */
+public class Episode {
+
+    //attributes for tvdb
+    private int seriesID;
+    
+}
---
+++ app/src/main/java/de/augsburg/seriesmarker2/Season.java
@@ -0,0 +1,7 @@
+package de.augsburg.seriesmarker2;
+
+/**
+ * Created by Tobias on 07.09.2015.
+ */
+public class Season {
+}
